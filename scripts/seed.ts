b7be036b856cb2e1cb5a61d1a991534a56c27cf5
vincenter <vincenterr@yahoo.com.tw>
2024-08-14 19:05:03 +0800

    Create a sqlite database

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,27 +1,58 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { db } from 'api/src/lib/db'
 
 // Manually apply seeds via the `yarn rw prisma db seed` command.
-//
-// Seeds automatically run the first time you run the `yarn rw prisma migrate dev`
-// command and every time you run the `yarn rw prisma migrate reset` command.
-//
-// See https://redwoodjs.com/docs/database-seeds for more info
+
+const POSTS = [
+  {
+    id: 1,
+    title: 'Welcome to the blog!',
+    body: "I'm baby single- origin coffee kickstarter lo - fi paleo skateboard.Tumblr hashtag austin whatever DIY plaid knausgaard fanny pack messenger bag blog next level woke.Ethical bitters fixie freegan,helvetica pitchfork 90's tbh chillwave mustache godard subway tile ramps art party. Hammock sustainable twee yr bushwick disrupt unicorn, before they sold out direct trade chicharrones etsy polaroid hoodie. Gentrify offal hoodie fingerstache.",
+  },
+  {
+    id: 2,
+    title: 'A little more about me',
+    body: "Raclette shoreditch before they sold out lyft. Ethical bicycle rights meh prism twee. Tote bag ennui vice, slow-carb taiyaki crucifix whatever you probably haven't heard of them jianbing raw denim DIY hot chicken. Chillwave blog succulents freegan synth af ramps poutine wayfarers yr seitan roof party squid. Jianbing flexitarian gentrify hexagon portland single-origin coffee raclette gluten-free. Coloring book cloud bread street art kitsch lumbersexual af distillery ethical ugh thundercats roof party poke chillwave. 90's palo santo green juice subway tile, prism viral butcher selvage etsy pitchfork sriracha tumeric bushwick.",
+  },
+  {
+    id: 3,
+    title: 'What is the meaning of life?',
+    body: 'Meh waistcoat succulents umami asymmetrical, hoodie post-ironic paleo chillwave tote bag. Trust fund kitsch waistcoat vape, cray offal gochujang food truck cloud bread enamel pin forage. Roof party chambray ugh occupy fam stumptown. Dreamcatcher tousled snackwave, typewriter lyft unicorn pabst portland blue bottle locavore squid PBR&B tattooed.',
+  },
+]
 
 export default async () => {
   try {
-    // Create your database records here! For example, seed some users:
-    //
-    // const users = [
-    //   { name: 'Alice', email: 'alice@redwoodjs.com },
-    //   { name: 'Bob', email: 'bob@redwoodjs.com },
-    // ]
-    //
-    // await db.user.createMany({ data: users })
+    await db.user.upsert({
+      where: { id: 1 },
+      create: {
+        id: 1,
+        name: 'John Doe',
+        email: 'admin@admin.com',
+        hashedPassword:
+          'ad9563042fe9f154419361eeeb775d8a12f3975a3722953fd8e326dd108d5645',
+        salt: '1c99de412b219e9abf4665293211adce',
+      },
+      update: {},
+    })
 
-    console.info(
-      '\n  No seed data, skipping. See scripts/seed.ts to start seeding your database!\n'
-    )
+    for (const post of POSTS) {
+      await db.post.upsert({
+        where: { id: post.id },
+        create: { ...post },
+        update: {},
+      })
+
+      console.log(`  Seeded "${post.title}"`)
+    }
+
+    console.info('')
+    console.info('  Seeded admin user:')
+    console.info('')
+    console.info('    Email: admin@admin.com')
+    console.info('    Password: admin')
+    console.info('')
+    console.info(`  (Please don't use this login in a production environment)`)
+    console.info('')
   } catch (error) {
     console.error(error)
   }
