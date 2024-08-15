4a9b18181fa1ba5305a32710bf20e9b6aa9531f8
vincenter <vincenterr@yahoo.com.tw>
2024-08-15 09:34:34 +0800

    Create tests

--- api/src/services/contacts/contacts.test.ts
+++ api/src/services/contacts/contacts.test.ts
@@ -30,22 +30,26 @@ describe('contacts', () => {
 
   scenario('creates a contact', async () => {
     const result = await createContact({
-      input: { name: 'String', email: 'String', message: 'String' },
+      input: {
+        name: 'Jane Doe',
+        email: 'jane@anonymous.com',
+        message: 'RedwoodJS is the best',
+      },
     })
 
-    expect(result.name).toEqual('String')
-    expect(result.email).toEqual('String')
-    expect(result.message).toEqual('String')
+    expect(result.name).toEqual('Jane Doe')
+    expect(result.email).toEqual('jane@anonymous.com')
+    expect(result.message).toEqual('RedwoodJS is the best')
   })
 
   scenario('updates a contact', async (scenario: StandardScenario) => {
     const original = (await contact({ id: scenario.contact.one.id })) as Contact
     const result = await updateContact({
       id: original.id,
-      input: { name: 'String2' },
+      input: { name: 'Johnathan Doe' },
     })
 
-    expect(result.name).toEqual('String2')
+    expect(result.name).toEqual('Johnathan Doe')
   })
 
   scenario('deletes a contact', async (scenario: StandardScenario) => {
